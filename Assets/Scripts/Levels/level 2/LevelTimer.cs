5e8ba7fe2e7e7dc66d5f348975134e333d233178
buznegosantiago <buznegosantiago@gmail.com>
2026-07-06 05:28:41 -0500

    Timer, Linterna, Restricciones de vision.

---
+++ Assets/Scripts/Levels/level 2/LevelTimer.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+public class LevelTimer : MonoBehaviour
+{
+    
+    [Header("Timer")]
+    [SerializeField] private TMP_Text clockText;
+    [SerializeField] private float timeToWin = 180f;
+
+    [Header("Scene")]
+    [SerializeField] private string winSceneName = "Win";
+
+    private float elapsedTime = 0f;
+    private bool levelFinished = false;
+
+    private void Update()
+    {
+        if (levelFinished) return;
+
+        elapsedTime += Time.deltaTime;
+
+        UpdateClockText();
+
+        if (elapsedTime >= timeToWin)
+        {
+            WinLevel();
+        }
+    }
+
+    private void UpdateClockText()
+    {
+        int totalSeconds = Mathf.FloorToInt(elapsedTime);
+        int minutes = totalSeconds / 60;
+        int seconds = totalSeconds % 60;
+
+        clockText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+
+    private void WinLevel()
+    {
+        levelFinished = true;
+        SceneManager.LoadScene(winSceneName);
+    }
+}
+
--- Assets/Scripts/Levels/level 2/ViewController.cs
+++ Assets/Scripts/Levels/level 2/ViewController.cs
@@ -18,7 +18,10 @@ public class ViewController : MonoBehaviour
 
     [Header("Tablet")]
     [SerializeField] private GameObject tabletPanel;
+    [Header("Linterna")]
+    [SerializeField] private GameObject flashlightEffect;
 
+    private bool flashlightOn = false;
     private RoomView currentView = RoomView.Central;
     private bool tabletOpen = false;
     private void Start()
@@ -28,6 +31,11 @@ public class ViewController : MonoBehaviour
         if (tabletPanel != null)
         {
             tabletPanel.SetActive(false);
+        }
+
+        if (flashlightEffect != null)
+        {
+            flashlightEffect.SetActive(false);
         }
     }
 
@@ -81,6 +89,11 @@ public class ViewController : MonoBehaviour
 
     public void OpenTablet()
     {
+        if (!CanOpenTablet())
+        {
+            return;
+        }
+
         tabletOpen = true;
         tabletPanel.SetActive(true);
     }
@@ -88,28 +101,82 @@ public class ViewController : MonoBehaviour
     private void CloseTablet()
     {
         tabletOpen = false;
-        tabletPanel.SetActive(false);
+
+        if (tabletPanel != null)
+        {
+            tabletPanel.SetActive(false);
+        }
+            
     }
 
     public void ToggleTablet()
     {
+
         if (tabletOpen)
         {
             CloseTablet();
+            return;
         }
-        else
-        {
-            OpenTablet();
-        }
+        
+        OpenTablet();
+        
     }
 
     private void UpdateView(RoomView newView)
     {
+        TurnOffFlashlight();
+        //s
+        CloseTablet();
         currentView = newView;
 
         centralView.SetActive(currentView == RoomView.Central);
         leftView.SetActive(currentView == RoomView.Left);
         rightView.SetActive(currentView == RoomView.Right);
         backView.SetActive(currentView == RoomView.Back);
+    }
+
+    public void ToggleFlashlight()
+    {
+        if (!CanUseFlashlight())
+        {
+            TurnOffFlashlight();
+            return;
+        }
+
+        flashlightOn = !flashlightOn;
+        flashlightEffect.SetActive(flashlightOn);
+
+        if (flashlightOn)
+        {
+            ScareAnimatronics();
+        }
+    }
+
+    public void TurnOffFlashlight()
+    {
+        flashlightOn = false;
+
+        if (flashlightEffect != null)
+        {
+            flashlightEffect.SetActive(false);
+        }
+    }
+
+    private bool CanUseFlashlight()
+    {
+        //Este bool identifica si se puede usar la linterna. En su defecto solo en vistas laterales o trasera
+        return currentView == RoomView.Left ||
+               currentView == RoomView.Right ||
+               currentView == RoomView.Back;
+    }
+    //s
+    private bool CanOpenTablet()
+    {
+        return currentView == RoomView.Central;
+    }
+
+    private void ScareAnimatronics()
+    {
+        Debug.Log("Linterna activada: aqui se alejaria al animatronico.");
     }
 }
